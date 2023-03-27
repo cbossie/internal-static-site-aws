1873d9a8e9bb3eab2ef72d9a8a56b861e6343bcb
Craig <cbossie@gmail.com>
2023-03-27 00:17:31 -0400

    Update

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -1,3 +1,36 @@
 resource "aws_s3_bucket" "spa_bucket" {
   bucket_prefix = var.bucket_prefix
 }
+
+resource "aws_s3_bucket_policy" "vpce_policy" {
+  bucket = aws_s3_bucket.spa_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "VPCEPOLICY"
+
+
+    Statement = [
+      {
+        Sid       = "Access-to-specific-VPCE-only"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = [aws_s3_bucket.spa_bucket.arn, "${aws_s3_bucket.spa_bucket.arn}/*"]
+        Condition = {
+          StringEquals = {
+            "aws:SourceVpce" = "${aws_vpc_endpoint.s3_endpoint.id}"
+          }
+        }
+      }
+    ]
+
+  })
+}
+
+
+
+
+
+
+
+
+
