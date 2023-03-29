6fc65ef656c5991ca229af1b7352fd6af7f5c724
Craig <cbossie@gmail.com>
2023-03-28 21:21:37 -0400

    Module

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -2,7 +2,8 @@ locals {
   azs        = ["${var.region}a", "${var.region}b", "${var.region}c"]
   s3_service = "com.amazonaws.${var.region}.s3"
   cidr       = "10.0.0.0/16"
-
+  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 
@@ -12,8 +13,8 @@ module "vpc" {
   azs                  = local.azs
   name                 = "proxy-vpc"
   cidr                 = local.cidr
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets      = local.private_subnets
+  public_subnets       = local.public_subnets
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_support   = true
